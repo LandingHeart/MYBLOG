c052d21f82d27e47d19ce20159d24d50b7e1ae16
LandingHeart <land.liu0123@gmail.com>
2019-01-10 21:13:21 -0500

    files

---
+++ src/components/slideShow.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+
+const Slideshow = () => {
+    return (
+      <Slide {...properties}>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+            <span>Slide 1</span>
+          </div>
+        </div>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+            <span>Slide 2</span>
+          </div>
+        </div>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+            <span>Slide 3</span>
+          </div>
+        </div>
+      </Slide>
+    )
+  }
+  const slideImages = [
+    'Images/slide01.JPG',
+    'Images/slide02.JPG'
+  ];
+  const properties = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    arrows: true
+  }
+  
